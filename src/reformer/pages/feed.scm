6b08e55cfb3de21d5c1afd2b42f077fab20a5072
James Orson <jamesaorson@gmail.com>
2024-08-11 08:10:51 -0700

    Add nested data to the feed

--- src/reformer/pages/feed.scm
+++ src/reformer/pages/feed.scm
@@ -1,27 +1,43 @@
 (define-module (reformer pages feed)
   #:export (index))
 
-(use-modules (web request)
+(use-modules (sxml simple)
+             (web request)
              (web response)
              (web uri)
-             (reformer html)
-             (sxml simple)
-             (reformer components navbar))
-
-(define posts '(((name . "A post") (content . "Lorem ipsum odor amet, consectetuer adipiscing elit. Dictum id penatibus facilisi commodo accumsan odio. Venenatis dis sollicitudin elementum torquent facilisis aenean hac feugiat metus."))
-                ((name . "Another post") (content . "Lorem ipsum odor amet, consectetuer adipiscing elit."))))
+             (reformer components navbar)
+             (reformer html))
 
 (define (get-field field alist)
   (cdr (assoc field alist)))
 
+(define posts '(((name . "A post")
+                 (content . "Lorem ipsum odor amet, consectetuer adipiscing elit. Dictum id penatibus facilisi commodo accumsan odio. Venenatis dis sollicitudin elementum torquent facilisis aenean hac feugiat metus.")
+                 (poster . ((handle . "OtherGuy"))))
+                ((name . "Another post")
+                 (content . "Lorem ipsum odor amet, consectetuer adipiscing elit.")
+                 (poster . ((handle . "SomeGuy"))))))
+
+(define (post-get-content post)
+  (get-field 'content post))
+
+(define (post-get-name post)
+  (get-field 'name post))
+
+(define (post-get-poster-handle post)
+  (get-field 'handle
+             (get-field 'poster post)))
+
 (define (index)
   (html-page
-    `(,(navbar)
-        (h1 (a (@ (href "/feed")) "Reformer Feed"))
-        ,(map 
-          (lambda (post)
-            `(div
-              (h3 ,(get-field 'name post))
-              (p ,(get-field 'content post))
-            ))
-          posts))))
+   `(,(navbar)
+     (h1 (a (@ (href "/feed"))
+            "Reformer Feed"))
+     ,(map 
+       (lambda (post)
+         `(div
+           (h3 ,(post-get-name post))
+           (p ,(post-get-content post))
+           (h4 ,(format #f "@~a" (post-get-poster-handle post)))
+           ))
+       posts))))
